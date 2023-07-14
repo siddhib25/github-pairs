4ff2cacb9db743c9233b6e4ce00d32362cdd3c6b
siddhi <bsiddhi01@gmail.com>
2023-07-14 10:37:58 -0400

    Initial Commit

---
+++ github pairs/github_pairsApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_pairsApp.swift
+//  github pairs
+//
+//  Created by Siddhi B on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct github_pairsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
